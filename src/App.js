1caed824771289220ab431e6f2f313f2598a82eb
Asijit <asijit00@gmail.com>
2022-08-23 21:22:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import axios from 'axios';
+import CopyToClipboard from 'react-copy-to-clipboard';
+import { AiTwotoneCopy } from 'react-icons/ai';
 
-function App() {
+export default function App() {
+  const [url, setUrl] = useState('');
+  const [userInput, setUserInput] = useState('');
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.shrtco.de/v2/shorten?url=${userInput}`
+      );
+
+      setUrl(response.data.result.full_short_link);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        placeholder="Enter link to be shortened"
+        onChange={(e) => {
+          setUserInput(e.target.value);
+        }}
+      />
+      <button onClick={fetchData}>SUBMIT</button>
+
+      <div>
+        <textarea type="text" readOnly={true} value={url} />
+      </div>
+      <CopyToClipboard text={url}>
+        <button>
+          <AiTwotoneCopy />
+        </button>
+      </CopyToClipboard>
     </div>
   );
 }
-
-export default App;
